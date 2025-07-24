7bacbc19b058398e13d0f84fd1c00e87d829df0c
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2025-07-24 21:50:02 +0800

    add providers

---
+++ src/providers.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { OnchainKitProvider } from '@coinbase/onchainkit'
+import type { ReactNode } from 'react'
+import { base } from 'wagmi/chains'
+
+export function Providers(props: { children: ReactNode }) {
+  return (
+    <OnchainKitProvider
+      apiKey={import.meta.env.VITE_PUBLIC_ONCHAINKIT_API_KEY}
+      chain={base}
+    >
+      {props.children}
+    </OnchainKitProvider>
+  )
+}
