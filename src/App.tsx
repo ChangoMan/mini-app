f1c586611dc3d12981da76b92f14e14879569fd7
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2025-07-24 11:27:19 +0800

    adding onchain kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { Earn } from '@coinbase/onchainkit/earn'
+import {
+  Address,
+  Avatar,
+  EthBalance,
+  Identity,
+  Name,
+} from '@coinbase/onchainkit/identity'
+import {
+  ConnectWallet,
+  Wallet,
+  WalletDropdown,
+  WalletDropdownDisconnect,
+} from '@coinbase/onchainkit/wallet'
 import { sdk } from '@farcaster/frame-sdk'
 import { useEffect } from 'react'
-import { parseEther } from 'viem'
-import {
-  type BaseError,
-  useAccount,
-  useConnect,
-  useDisconnect,
-  useSendTransaction,
-  useWaitForTransactionReceipt,
-} from 'wagmi'
 
 function App() {
   useEffect(() => {
@@ -16,97 +21,93 @@ function App() {
   }, [])
 
   return (
-    <div className="max-w-xl mx-auto p-6 h-screen bg-gray-900 text-white overflow-x-hidden">
-      <h1 className="text-2xl font-bold text-center">Hunter Chang</h1>
+    <div className="max-w-xl mx-auto py-12 px-6 h-screen bg-gray-900 text-white overflow-x-hidden">
+      <h1 className="text-center text-2xl font-semibold">
+        Easily earn interest on your USDC
+      </h1>
       <div className="mt-6 text-center">
-        <ConnectMenu />
+        <EarnSection />
       </div>
     </div>
   )
 }
 
-function ConnectMenu() {
-  const { isConnected, address } = useAccount()
-  const { connect, connectors } = useConnect()
-
-  if (isConnected) {
-    return (
-      <>
-        <div>Connected account:</div>
-        <div className="text-xs whitespace-wrap break-words">{address}</div>
-        <SignButton />
-      </>
-    )
-  }
-
+function EarnSection() {
   return (
-    <div className="flex flex-col gap-2 max-w-xs mx-auto">
-      {connectors.map((connector) => (
-        <button
-          key={connector.uid}
-          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
-          onClick={() => connect({ connector })}
-        >
-          Connect with {connector.name}
-        </button>
-      ))}
+    <div className="flex flex-col items-center justify-center gap-6">
+      <Wallet>
+        <ConnectWallet>
+          <Avatar className="h-6 w-6" />
+          <Name />
+        </ConnectWallet>
+        <WalletDropdown>
+          <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
+            <Avatar />
+            <Name />
+            <Address />
+            <EthBalance />
+          </Identity>
+          <WalletDropdownDisconnect />
+        </WalletDropdown>
+      </Wallet>
+      <Earn vaultAddress="0x7BfA7C4f149E7415b73bdeDfe609237e29CBF34A" />
     </div>
   )
 }
 
-function SignButton() {
-  const {
-    data: hash,
-    error: sendError,
-    isPending: isSendPending,
-    sendTransaction,
-  } = useSendTransaction()
-  const { disconnect } = useDisconnect()
+// function SendButton() {
+//   const {
+//     data: hash,
+//     error: sendError,
+//     isPending: isSendPending,
+//     sendTransaction,
+//   } = useSendTransaction()
+//   const { disconnect } = useDisconnect()
 
-  async function handleSendEth() {
-    sendTransaction({
-      to: '0xf7e89E45502890381F9242403eA8661fad89Ca79', //hunterchang.eth
-      value: parseEther('0.0002'),
-    })
-  }
+//   async function handleSendEth() {
+//     sendTransaction({
+//       to: '0xf7e89E45502890381F9242403eA8661fad89Ca79', //hunterchang.eth
+//       value: parseEther('0.0002'),
+//     })
+//   }
 
-  const { isLoading: isConfirming, isSuccess: isConfirmed } =
-    useWaitForTransactionReceipt({
-      hash,
-    })
+//   const { isLoading: isConfirming, isSuccess: isConfirmed } =
+//     useWaitForTransactionReceipt({
+//       hash,
+//     })
 
-  return (
-    <>
-      <div className="mt-12">
-        <h2 className="mb-0 text-xl font-bold">Send Hunter ETH</h2>
-        <button
-          type="button"
-          disabled={isSendPending}
-          onClick={handleSendEth}
-          className="mt-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
-        >
-          {isSendPending ? 'Confirming...' : 'Send 0.0002 ETH'}
-        </button>
-        {hash && <div>Transaction Hash: {hash}</div>}
-        {isConfirming && <div>Waiting for confirmation...</div>}
-        {isConfirmed && <div>Transaction confirmed.</div>}
-        {sendError && (
-          <div>
-            Error: {(sendError as BaseError).shortMessage || sendError.message}
-          </div>
-        )}
-      </div>
+//   return (
+//     <>
+//       <div className="mt-12">
+//         <h1 className="mb-0 text-xl font-bold">Send Hunter ETH</h1>
+//         <button
+//           type="button"
+//           disabled={isSendPending}
+//           onClick={handleSendEth}
+//           className="mt-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+//         >
+//           {isSendPending ? 'Confirming...' : 'Send 0.0002 ETH'}
+//         </button>
+//         {hash && <div>Transaction Hash: {hash}</div>}
+//         {isConfirming && <div>Waiting for confirmation...</div>}
+//         {isConfirmed && <div>Transaction confirmed.</div>}
+//         {sendError && (
+//           <div>
+//             Error: {(sendError as BaseError).shortMessage || sendError.message}
+//           </div>
+//         )}
+//       </div>
 
-      <div className="mt-12">
-        <button
-          className="bg-pink-500 hover:bg-pink-600 text-white text-sm px-3.5 py-1.5 rounded"
-          onClick={() => disconnect()}
-        >
-          Disconnect
-        </button>
-      </div>
-    </>
-  )
-}
+//       <div className="mt-12">
+//         <button
+//           className="bg-pink-500 hover:bg-pink-600 text-white text-sm px-3.5 py-1.5 rounded"
+//           onClick={() => disconnect()}
+//         >
+//           Disconnect
+//         </button>
+//       </div>
+//     </>
+//   )
+// }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,16 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { WagmiProvider } from "wagmi";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import App from "./App.tsx";
-import { config } from "./wagmi.ts";
+import App from './App.tsx'
+import { Providers } from './providers'
 
-import "./index.css";
+import '@coinbase/onchainkit/styles.css'
+import './index.css'
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </WagmiProvider>
-  </React.StrictMode>,
-);
+    <Providers>
+      <App />
+    </Providers>
+  </React.StrictMode>
+)
